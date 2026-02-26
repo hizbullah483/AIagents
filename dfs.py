7d8096aeed4790ccaf70427b4bfbe098d17a3ebb
hizbullah483 <hizbullahrehman483@gmail.com>
2026-02-26 20:52:09 +0500

    Implement depth-first search algorithm

---
+++ dfs.py
@@ -0,0 +1,30 @@
+graph = {
+    "Entrance" : ["hallwayA","hallwayB"],
+    "hallwayA" : ["storage"],
+    "hallwayB" : ["target"],
+    "target" : [],
+    "deepvault" : [],
+    "storage" : ["deepvault"]
+}
+
+limit = 2
+stack = [[0,["Entrance"]]]
+visited = []
+
+while len(stack) > 0:
+    currentdepth,currentpath = stack.pop()
+    if currentdepth > limit:
+        print("depth exceeded. mission failed")
+        break
+    currentlocation = currentpath[-1]
+
+    if currentlocation == "target":
+        print(currentpath," at depth ",currentdepth)
+        break
+
+    if currentlocation not in visited:
+        visited.append(currentlocation)
+        for neighbour in graph.get(currentlocation):
+            newpath = currentpath + [neighbour]
+            stack.append((currentdepth+1,newpath))
+
